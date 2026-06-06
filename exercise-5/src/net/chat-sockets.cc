c585614325c13d84f4a452a1494fb049de707599
Anurag <anurag4629@gmail.com>
2026-06-06 12:03:31 +0000

    abstract address and socket

--- exercise-5/src/net/chat-sockets.cc
+++ exercise-5/src/net/chat-sockets.cc
@@ -12,3 +12,68 @@ sockaddr_in tt::chat::net::create_address(int port) {
   address.sin_port = htons(port);
   return address;
 }
+
+namespace tt::chat::net{
+  Socket::Socket(){
+    socket_fd_ = socket(AF_INET,SOCK_STREAM,0);
+    tt::chat::check_error(socket_fd_ < 0, "Socket creation error\n");
+  }
+
+  Socket::Socket(int fd): socket_fd_(fd){}
+
+  Socket::~Socket(){
+    close_sock();
+  }
+
+  Socket::Socket(Socket&& other) noexcept{
+    socket_fd_ = other.socket_fd_;
+    other.socket_fd_ = -1;
+  }
+
+  Socket& Socket::operator=(Socket&& other) noexcept{
+    if(this == &other) return *this;
+    
+    close_sock();
+    socket_fd_ = other.socket_fd_;
+    other.socket_fd_ = -1;
+
+    return *this;
+  }
+
+  int Socket::get() const{
+    return socket_fd_;
+  }
+
+  bool Socket::is_valid() const{
+    return (socket_fd_ != -1);
+  }
+
+  void Socket::close_sock(){
+    if(socket_fd_ != -1) close(socket_fd_);
+    socket_fd_ = -1;
+  }
+
+  Address::Address(int port){
+    create_address(port);
+    address_.sin_addr.s_addr = INADDR_ANY;
+  }
+
+  Address::Address(const std::string &server_ip, int port){
+    create_address(port);
+    auto err_code = inet_pton(AF_INET,server_ip.c_str(), &address_.sin_addr);
+    check_error(err_code <= 0, "Invlaid address/ Address not supported\n"); 
+  }
+
+  void Address::create_address(int port){
+    address_.sin_family = AF_INET;
+    address_.sin_port = htons(port);  
+  }
+
+  const sockaddr* Address::as_sockaddr() const{
+    return reinterpret_cast<const sockaddr*>(&address_);
+  }
+
+  socklen_t Address::size() const{
+    return sizeof(address_);
+  }
+}
--- exercise-5/src/net/chat-sockets.h
+++ exercise-5/src/net/chat-sockets.h
@@ -12,8 +12,41 @@
 namespace tt::chat::net {
 
 int create_socket();
-
 sockaddr_in create_address(int port);
+
+class Socket{
+public:
+    Socket();
+    explicit Socket(int fd);
+
+    ~Socket();
+
+    Socket(const Socket& other) = delete;
+    Socket& operator=(const Socket& other) = delete;
+
+    Socket(Socket&& other) noexcept;
+    Socket& operator=(Socket&& other) noexcept;
+
+    int get() const;
+    bool is_valid() const;
+    void close_sock();
+
+private:
+    int socket_fd_;
+};
+
+class Address{
+public:
+    explicit Address(int port); //server ctor
+    Address(const std::string &server_ip, int port); //client ctor
+
+    const sockaddr* as_sockaddr() const;
+    socklen_t size() const;
+    void create_address(int port);
+
+private:
+    sockaddr_in address_{};
+};
 
 } // namespace tt::chat::net
 
